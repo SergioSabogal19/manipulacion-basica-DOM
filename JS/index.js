00be2db7c13b959e13000f96c7ce43a20bec4466
Sergio Sabogal <sdsabogalq@correo.udistrital.edu.co>
2023-08-25 14:43:07 -0500

    integracion de boton

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,11 @@
 const titulo_Pagina = document.querySelector('h1');
 const number_A = document.getElementById('value_A');
+const number_B = document.getElementById('value_B');
 const name = document.getElementsByClassName('form--name');
-const contenedor_Img = document.getElementsByClassName('container--img')
+const contenedor_Img = document.getElementsByClassName('container--img');
 const formulario = document.getElementsByClassName('form');
+const impresion_Resultado = document.getElementById('resultado');
+
 console.log(number_A.value);
 console.log ({
     titulo_Pagina,
@@ -31,5 +34,16 @@ titulo_Pagina.classList.remove('title');
     event.preventDefault();
     const imagen_Respuesta  = document.createElement('img');
     imagen_Respuesta.setAttribute('src', '../img/wakandaPruebaP.jpeg');
+    imagen_Respuesta.setAttribute('class','form--img')
     contenedor_Img[0].appendChild(imagen_Respuesta);
+    const resultado = Number(number_A.value) + Number(number_B.value);
+    impresion_Resultado.innerText = resultado;
+    
+    const button_Delete = document.createElement('button');
+    button_Delete.setAttribute('class', 'form--btn');
+    button_Delete.innerText = 'Borrar';
+
+    formulario[0].appendChild(button_Delete);
+
+
  } 
